746b2615b1896dae11f67a660a1b8f4a2a563d63
robomancy <93403866+FabioFlorey@users.noreply.github.com>
2024-01-10 15:02:42 +0100

    Create parse_html.py

---
+++ lib/parse_html.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+# *-* mode: python; coding: utf-8 *-*
+"""
+=============================================================================
+Watchtower
+=============================================================================
+HTML Checksum.
+"""
+from __future__ import annotations
+
+import requests
+import hashlib
+import random
+import string
+
+from bs4 import BeautifulSoup
+
+def generate_random_characters(length=15):
+    characters = string.ascii_letters + string.digits
+    random_chars = ''.join(random.choice(characters) for _ in range(length))
+    return random_chars
+
+def get_html(url):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        html_content = response.content
+        
+        soup = BeautifulSoup(html_content, 'lxml')
+        
+        for tag in soup.find_all(True):
+            attrs = ['class','id','href', 'value']
+            [tag.attrs.pop(_, None) for _ in attrs]
+        
+        for script_tag in soup.find_all('script'):
+            script_tag.decompose()
+        
+        modified_html = str(soup)
+        
+        return hashlib.sha256(modified_html.encode()).hexdigest()
+    else:
+        return f'Error > {generate_random_characters()}'
